9b2041ca3755b78e32228ca1cde9453a6a9c2551
Brenton Niebauer <b.niebauer@me.com>
2026-03-13 11:12:39 -0400

    First 2 projects

---
+++ CheckPalindrome/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string input = "tacocat";
+        char[] arr = input.ToCharArray();
+        Array.Reverse(arr);
+        string result = new string(arr);
+
+        if (input == result)
+        {
+            Console.WriteLine($"{input} is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine($"{input} is not a palindrome.");
+        }
+    }
+}
---
+++ string-reverse/ReverseString/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string input = "ReverseMe";
+        Console.WriteLine($"The input to be reversed: {input}");
+        char[] arr = input.ToCharArray();
+        Array.Reverse(arr);
+        string result = new string(arr);
+
+        Console.WriteLine($"Reversed: {result}");
+    }
+}

4476cb4994abdb2f3696f9ebd7fb43a59ae5cc06
Brenton Niebauer <b.niebauer@me.com>
2026-03-14 11:01:16 -0400

    add prime project

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int num = 7;
+        bool isPrime = true;
+
+        for (int i = 2; i <= num / 2; i++)
+        {
+            if (num % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        Console.WriteLine($"{num} is " + (isPrime ? "Prime" : "not Prime"));
+    }
+}

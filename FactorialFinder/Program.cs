24a36aff3083946f1c6755d7153d175aef70a6a0
Brenton Niebauer <b.niebauer@me.com>
2026-03-14 10:38:37 -0400

    Add factorial and fibonacci projects

---
+++ FactorialFinder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CommandLine;
+using System.Threading.Tasks;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var numOption = new Option<int>("--num", "Number");
+
+        var rootCommand = new RootCommand { numOption };
+
+        rootCommand.SetAction((num) =>
+        {
+            Console.WriteLine(num);
+        });
+        int num = 5;
+        int fact = 1;
+
+        for (int i = 1; i <= num; i++)
+        {
+            fact *= i;
+        }
+
+        Console.WriteLine(fact);
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = 20, a = 0, b = 1, c;
+
+        Console.Write(a + " ");
+
+        for (int i = 2; i < n; i++)
+        {
+            c = a + b;
+            Console.Write(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+}

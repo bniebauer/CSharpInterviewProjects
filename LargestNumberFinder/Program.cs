ee8d09484c6cf4fbde5a1b785d113c46666c2c61
Brenton Niebauer <b.niebauer@me.com>
2026-03-14 10:48:22 -0400

    Add LargestNumber project

---
+++ LargestNumberFinder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 10, 25, 5, 40, 15 };
+        Console.WriteLine("[{0}]", string.Join(", ", arr) );
+        int max = arr[0];
+
+        foreach (int num in arr)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        Console.WriteLine($"The largest number in the array is: {max}!");
+    }
+}
